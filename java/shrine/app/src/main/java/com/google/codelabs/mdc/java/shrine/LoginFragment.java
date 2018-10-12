82a7d67c9aae3f2bd1384d4fad9bb7e7ebf0be95
Parth Joshi <parth.a.joshi@accenture.com>
2018-10-12 12:00:18 +0530

    MDC 101 Codelab complete.

--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
@@ -2,7 +2,13 @@ package com.google.codelabs.mdc.java.shrine;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.button.MaterialButton;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,10 +24,44 @@ public class LoginFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.shr_login_fragment, container, false);
 
+        final TextInputLayout passwordLayout = view.findViewById(R.id.password_text_input);
+        final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
+        MaterialButton nextButton = view.findViewById(R.id.next_button);
+
+        // Set an error if the password is less than 8 characters.
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isPasswordValid(passwordEditText.getText()))
+                    passwordLayout.setError(getString(R.string.shr_error_password));
+                else {
+                    passwordLayout.setError(null);
+
+                    // Navigate to next fragment
+                    ((NavigationHost)getActivity())
+                            .navigateTo(new ProductGridFragment(), false);
+                }
+            }
+        });
+
+        // Clear the error once more than 8 characters are typed.
+        passwordEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(isPasswordValid(passwordEditText.getText()))
+                    passwordLayout.setError(null);
+
+                return false;
+            }
+        });
+
         // Snippet from "Navigate to the next Fragment" section goes here.
 
         return view;
     }
 
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+    private boolean isPasswordValid(@Nullable Editable text){
+        return (text != null && text.length() >= 8);
+    }
 }
